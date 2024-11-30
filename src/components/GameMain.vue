d7e724c386bb02742daf3f7124fdf0cfdba5ce25
jacopolegovini <legovini96@gmail.com>
2024-11-30 16:35:54 +0100

    click method added

--- src/components/GameMain.vue
+++ src/components/GameMain.vue
@@ -2,7 +2,18 @@
 export default {
     data() {
         return {
+            squares: Array(36).fill(null),
+            sunClicked: false,
+            moonClicked: false,
+            currentIndex: null,
 
+        }
+    },
+    methods: {
+        showSun(index) {
+            console.log(index)
+            this.currentIndex = index
+            return this.sunClicked = !this.sunClicked
         }
     }
 }
@@ -11,8 +22,9 @@ export default {
 <template>
     <div class="general-container">
         <ul class="game-container">
-            <li v-for="square in 36" class="square">
-
+            <li v-for="(square, index) in squares" :key="index" class="square" @click="showSun(index)">
+                <div v-if="sunClicked && index === currentIndex" class="inner-square sun-click">
+                </div>
             </li>
         </ul>
     </div>
@@ -22,22 +34,28 @@ export default {
 .general-container {
     max-width: 1000px;
     margin: 0 auto;
-    height: 200px;
+    height: 600px;
 }
 
 .game-container {
-    height: 120px;
-    width: 120px;
+    height: 540px;
+    width: 540px;
     display: flex;
     flex-wrap: wrap;
 }
 
 .square {
-    flex-basis: 20px;
+    flex-basis: 90px;
+    height: 90px;
+}
+
+.inner-square {
+    height: 100%;
 }
 
 ul {
     list-style-type: none;
+    padding: 0;
 }
 
 li {
@@ -45,5 +63,11 @@ li {
     height: 20px;
     width: 20px;
     border: 1px solid gray;
+}
+
+.sun-click {
+    border: 1px solid orange;
+    background-color: yellow;
+    border-radius: 50%;
 }
 </style>
