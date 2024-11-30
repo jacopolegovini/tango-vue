90d05413e41ac622a302c96fa74a61d3c73a7f94
jacopolegovini <legovini96@gmail.com>
2024-11-30 13:38:51 +0100

    display flex implemented

--- src/components/GameMain.vue
+++ src/components/GameMain.vue
@@ -9,13 +9,41 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <ul class="row">
-            <li v-for="square in 36" class="col-2">
-                test
+    <div class="general-container">
+        <ul class="game-container">
+            <li v-for="square in 36" class="square">
+
             </li>
         </ul>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.general-container {
+    max-width: 1000px;
+    margin: 0 auto;
+    height: 200px;
+}
+
+.game-container {
+    height: 120px;
+    width: 120px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.square {
+    flex-basis: 20px;
+}
+
+ul {
+    list-style-type: none;
+}
+
+li {
+    padding: 0;
+    height: 20px;
+    width: 20px;
+    border: 1px solid gray;
+}
+</style>
